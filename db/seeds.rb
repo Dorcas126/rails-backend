8ce18f9cbce079b540f5360c4e3d828e29ced978
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-11-08 12:09:57 +0200

    Create greeting text

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(greeting_text: 'Hello')
+Greeting.create(greeting_text: 'Bonjour')
+Greeting.create(greeting_text: 'ca va?)')
+Greeting.create(greeting_text: 'Hope your are doing well')
+Greeting.create(greeting_text: 'Have a good day Raph')
